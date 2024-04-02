c339ebced1fd3c5e7d9896ce1474cccb6042c5a5
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-04-02 17:12:45 +0300

    first

---
+++ scripts/postInstall.sh
@@ -0,0 +1,17 @@
+#set env vars
+set -o allexport; source .env; set +o allexport;
+
+#wait until the server is ready
+echo "Waiting for software to be ready ..."
+sleep 60s;
+
+target=$(docker-compose port web 3000)
+
+curl http://${target}/api/admin/accounts/create \
+  -H 'accept: */*' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6,zh-CN;q=0.5,zh;q=0.4,ja;q=0.3' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H 'pragma: no-cache' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36' \
+  --data-raw '{"username":"admin","password":"'${ADMIN_PASSWORD}'"}'
---
+++ scripts/preInstall.sh
@@ -0,0 +1,11 @@
+#set env vars
+set -o allexport; source .env; set +o allexport;
+
+mkdir -p ./files
+chmod -R 777 ./files
+
+sid -i "s~URL_TO_CHANGE~${DOMAIN}~g" ./misskey/.config/default.yml
+sid -i "s~DB_USER_TO_CHANGE~${POSTGRES_USER}~g" ./misskey/.config/default.yml
+sid -i "s~DB_PASSWORD_TO_CHANGE~${POSTGRES_PASSWORD}~g" ./misskey/.config/default.yml
+
+docker-compose run --rm web pnpm run init
